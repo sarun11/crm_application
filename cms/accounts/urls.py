12bf0d922efb292da5e66f6aca59ec8d78f5182a
Sarun Dahal <sarundahal11@gmail.com>
2020-11-12 10:01:41 +0545

    adds accounts, products and customers app

--- cms/accounts/urls.py
+++ cms/accounts/urls.py
@@ -2,8 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('products/', views.products, name="products"),
-    path('about/', views.about, name="about")
-    
+    path('', views.accounts, name="accounts"),   
 ]
