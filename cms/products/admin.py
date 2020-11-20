689067b2ab1304f108cdc08b897110d6c04eebda
Sarun Dahal <sarundahal11@gmail.com>
2020-11-20 10:59:47 +0545

    makes changes to existing models

--- cms/products/admin.py
+++ cms/products/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from .models import Product
 from .models import Order
+from .models import Tag
 
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Tag)
--- cms/products/models.py
+++ cms/products/models.py
@@ -1,21 +1,38 @@
 from django.db import models
 
 # Create your models here
+from customer.models import Customer
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, null=True)
+    
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
+    
+    CATEGORY = (
+        ('Stationery', 'Stationery'),
+        ('Sports Items', 'Sports'),
+        ('Clothing', 'Clothing'),
+        ('Miscellaneous', 'Misc')
+    )
+    
     name = models.CharField(max_length=100, null=True)
     description = models.CharField(max_length=500, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=30, null=True)
+    category = models.CharField(max_length=30, null=True, choices=CATEGORY)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
     
 
 class Order(models.Model):
-    # customer =
-    # product = 
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     
     STATUS = (
         ('Pending', 'Pending'),
@@ -27,4 +44,4 @@ class Order(models.Model):
     status = models.CharField(max_length=25, null=True, choices=STATUS)
      
     def __str__(self):
-        return self.status
+        return str(self.customer) + "_" + str(self.product)
