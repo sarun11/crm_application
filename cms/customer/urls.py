5bdde5d52fe5cde11d19d6e19661f16685446300
Sarun Dahal <sarundahal11@gmail.com>
2020-11-22 13:09:46 +0545

    Renders Individual Customer pages

--- cms/customer/urls.py
+++ cms/customer/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.customer, name="customers")
+    path('<str:pk>', views.customer, name="customers")
 ]
--- cms/customer/views.py
+++ cms/customer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from customer.models import Customer
 # Create your views here.
 
-def customer(request):
-    return render(request, 'customer.html')
+def customer(request, pk):
+    
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    
+    total_orders = orders.count()
+    
+    context = {'customer': customer, 'total_orders': total_orders, 'orders': orders}
+    return render(request, 'customer.html', context)
