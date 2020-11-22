7e0f3acc01243ec3646a2b6e9dd66c5a685cf3b7
Sarun Dahal <sarundahal11@gmail.com>
2020-11-22 12:29:52 +0545

    rendered items into the frontend

--- cms/home/views.py
+++ cms/home/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from customer.models import Customer
+from products.models import Order
+from django.db.models import Q
 
 def home(request):
-    return render(request, "home.html")
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    
+    last_5 = Order.objects.all().order_by('-date_created')[:5]
+    
+    orders_delivered = Order.objects.filter(status='Delivered').count()
+    orders_pending = Order.objects.filter(~Q(status='Delivered')).count()
+    
+    return render(request, "home.html", 
+                  {'customer': customers, 
+                   'orders': orders, 
+                   'total_orders': total_orders,
+                   'orders_delivered': orders_delivered,
+                   'orders_pending': orders_pending,
+                   'last_5': last_5,
+                   })
--- cms/products/views.py
+++ cms/products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
 
 def products(request):
-    return render(request, 'products.html')
+    
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
