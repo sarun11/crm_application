e5cb293c254756af76f67524c78caacbdcefccb6
Sarun Dahal <sarundahal11@gmail.com>
2020-11-24 12:26:15 +0545

    adds place multiple order functionality from the customer page

--- cms/customer/urls.py
+++ cms/customer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<str:pk>/', views.customer, name="customers"),
+    path('create_order_/<str:pk>/', views.create_order_, name="create_order_customer"),
     path('delete_order_/<str:cid>/<str:pk>/', views.delete_order_, name="delete_order_"),
     path('update_order_/<str:cid>/<str:pk>/', views.update_order_, name="update_order_")
 ]
--- cms/customer/views.py
+++ cms/customer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from customer.models import Customer
 from products.models import Order
 from products.forms import OrderForm
+from django.forms import inlineformset_factory
 
 # Create your views here.
 
@@ -14,6 +15,37 @@ def customer(request, pk):
     
     context = {'customer': customer, 'total_orders': total_orders, 'orders': orders}
     return render(request, 'customer.html', context)
+
+
+def create_order_(request, pk):
+    
+    customer = Customer.objects.get(id=pk)
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra= 5)
+    
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    
+    if request.method == "POST":
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            path = '/customer/' + str(pk) + '/'
+            return redirect(path)
+    
+    context = {'customer': customer, 'formset': formset}
+    
+    return render(request, 'create_order_.html', context=context)
+    
+    '''customer = Customer.objects.get(id=pk)
+    form = OrderForm(initial={'customer': customer})
+    
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            path = '/customer/' + str(pk) + '/'
+            return redirect(path)
+    context = {'form': form, 'customer': customer}
+    return render(request, 'create_order.html', context=context)'''
 
 
 def delete_order_(request, cid, pk):
@@ -43,6 +75,5 @@ def update_order_(request, cid, pk):
             path = '/customer/' + str(cid) + '/'
             return redirect(path)
         
-    context = {'customer': customer, 'order': order, 'form': form}
-    
+    context = {'customer': customer, 'order': order, 'form': form}  
     return render(request, 'create_order.html', context=context)
